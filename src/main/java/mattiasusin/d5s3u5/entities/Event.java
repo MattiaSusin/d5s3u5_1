65a996f03498003c8f5685d449d67fd21fce1622
MattiaSusin <m.susin28@gmail.com>
2024-09-20 10:38:30 +0200

    Commit Class Event & EventsRepository

---
+++ src/main/java/mattiasusin/d5s3u5/entities/Event.java
@@ -0,0 +1,59 @@
+package mattiasusin.d5s3u5.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "events")
+@Setter
+@Getter
+@NoArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+
+    private String Title;
+    private String Description;
+    private int place_available;
+    private Date date;
+    private String place;
+
+    // ONE TO MANY --> PRENOTAZIONE
+
+    // COSTRUTTORI
+
+    public Event(String title, String description, int place_available, Date date, String place) {
+        Title = title;
+        Description = description;
+        this.place_available = place_available;
+        this.date = date;
+        this.place = place;
+    }
+
+
+    // TO STRING
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", Title='" + Title + '\'' +
+                ", Description='" + Description + '\'' +
+                ", place_available=" + place_available +
+                ", date=" + date +
+                ", place='" + place + '\'' +
+                '}';
+    }
+
+}
---
+++ src/main/java/mattiasusin/d5s3u5/repositories/EventsRepositoy.java
@@ -0,0 +1,11 @@
+package mattiasusin.d5s3u5.repositories;
+
+import mattiasusin.d5s3u5.entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EventsRepositoy extends JpaRepository<Event, UUID> {
+}
