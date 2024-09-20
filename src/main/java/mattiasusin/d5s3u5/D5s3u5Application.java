47602b581b7c508e18bc93342c133e88390f47ec
MattiaSusin <m.susin28@gmail.com>
2024-09-20 09:35:55 +0200

    Initial commit

---
+++ src/main/java/mattiasusin/d5s3u5/D5s3u5Application.java
@@ -0,0 +1,13 @@
+package mattiasusin.d5s3u5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D5s3u5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D5s3u5Application.class, args);
+	}
+
+}
---
+++ src/test/java/mattiasusin/d5s3u5/D5s3u5ApplicationTests.java
@@ -0,0 +1,13 @@
+package mattiasusin.d5s3u5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D5s3u5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
